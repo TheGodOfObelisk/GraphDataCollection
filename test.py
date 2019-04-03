6bab04dbe99a4535a378d36d744a11b65efc9fc7
TheGodOfObelisk <1198582189@qq.com>
2019-04-03 19:24:50 +0800

    collect software info

---
+++ test.py
@@ -0,0 +1,8 @@
+import json
+str1 = "asdkasjdhal {\"count\":199865,\"results\":[{\"vertexType\":\"software\",\"_type\":\"vertex\",\"source\":[\"Hone\"],\"processPath\":\"\/usr\/bin\/gnome-panel\",\"description\":\"\/usr\/bin\/gnome-panel\",\"name\":\"\/usr\/bin\/gnome-panel\",\"processArgs\":\"gnome-panel\",\"processPid\":2660,\"processPpid\":2401,\"_id\":2210304}],\"success\":true,\"version\":\"2.5.0\",\"queryTime\":2119.135322}"
+str2 = str1.split('{', 1)
+str3 = "{" + str2[1]
+print(str3)
+with open("./demo-data/test.json", "w") as f:
+    json.dump(str3, f)
+    print("write over")
--- testAPI.py
+++ testAPI.py
@@ -1,5 +1,16 @@
 import subprocess
-cmd = "curl -XGET -i -H 'accept-encoding: application/json' http://10.10.10.100:8000/api/search\?vertexType\=malware"
-sub = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-sub.wait()
-print sub.stdout.read()
+import json
+i = 0
+while i < 2000:# 199865 items in , set 2000 here
+    cmd = "curl -XGET -i -H 'accept-encoding: application/json' http://10.10.10.100:8000/api/search\?vertexType\=software\&pageSize\=100\&page\=" + str(i)
+    sub = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    sub.wait()
+    str1 = sub.stdout.read()
+    str1 = str1.decode()
+    str2 = str1.split('{', 1)
+    str3 = "{" + str2[1]
+    # print str1.split('{')
+    with open("./demo-data/software/software_" + str(i) + ".json", "w") as f:
+        json.dump(str3, f)
+        print("write " + str(i + 1) + " time(s)")
+    i += 1
