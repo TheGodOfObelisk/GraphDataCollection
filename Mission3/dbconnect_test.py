b2e846de91bc1b13899faf83b79177dc98cddacb
TheGodOfObelisk <1198582189@qq.com>
2019-05-15 14:57:51 +0800

    edit data

--- Mission3/dbconnect_test.py
+++ Mission3/dbconnect_test.py
@@ -18,6 +18,7 @@ def get_result_list(root_dir):
         return -1
     return dirs
 lists = []
+dict_length = {}
 db = MySQLdb.connect("localhost", "root", "123456", "net_test")
 cursor = db.cursor()
 # cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
@@ -33,8 +34,8 @@ cursor = db.cursor()
 miss_list = [u'', u'     Version', u' Note', u' Name', u' Version', u'NOTE ', 
 u'Adware.Look2Me', u'Simplocker', u' LIFEMONITOR', u'ALERTA', u'For APK',
 u'For Name', u'APK', u'APKs', u'Important', u'Keys', u'Optional', u'Varies'
-, u'Icon', u'ERROR', u'NOTE', u'Note']
-
+, u'Icon', u'ERROR', u'NOTE', u'Note', u'Message']
+miss_list1 = [ ]
 res_dirs = get_result_list("../Mission2/symantec/data/")
 # print res_dirs
 for dir_item in res_dirs:
@@ -43,10 +44,21 @@ for dir_item in res_dirs:
         for item in load_dict:
             key_list = item.keys()
             for i in key_list:
-                if i in miss_list:
+                if i in miss_list1:
                     print "strange property"
                     print i
                     print "it is in " + dir_item
+                if i in lists:
+                    if len(i) <= 20 and i not in miss_list:
+                        print dict_length
+                        print "the one that is coming"
+                        print i + ": " + str(len(item[i]))
+                        if i not in dict_length.keys():
+                            print "initialize length"
+                            dict_length[i] = len(item[i])#记录每种字段的最大长度
+                        elif i in dict_length.keys() and dict_length[i] < len(item[i]):
+                            print "update length"
+                            dict_length[i] = len(item[i])
                 if i not in lists:
                     # if len(i) > 20:
                     #     print "too long : " + i
@@ -55,10 +67,12 @@ for dir_item in res_dirs:
                     #     lists.append(i)
                     if len(i) <= 20 and i not in miss_list:
                         lists.append(i)
+                        # lists_with_length.append(i + ": " + str(len(item[i])))
                     ## 大于20的明显有误
 print "TOTAL:"
 print lists
-
+print dict_length
+# print lists_with_length
         # try:
         #     cursor.execute("""
         #     insert into `symantec_data` (`content`) values(:va);
